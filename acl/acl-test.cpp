d9b1c64d19b70c5cae7c4166b2c2a6731a067378
jiahaobi <jiahaobi@sohu-inc.com>
2024-11-15 12:21:27 +0800

    read code

--- acl/acl-test.cpp
+++ acl/acl-test.cpp
@@ -22,12 +22,21 @@ void xyz(acl::redis_string &cmd_string, acl::redis_key &cmd_key, bool &exitFlag)
 
 int main(int argc, char **argv) {
     static bool exitFlag = false;
-    cluster.set("10.18.150.158:6490", 0, 10, 120);
+    cluster.init(nullptr, "10.18.150.158:7509,10.18.150.27:7507,10.18.150.27:7508", 5, 10, 10);
     cluster.set_password("default", "fe1743934d1ea8006ce75c0c77c7c5e8");
     acl::redis_string cmd_string;
     acl::redis_key  cmd_key;
     cmd_key.set_cluster(&cluster);
     cmd_string.set_cluster(&cluster);
     xyz(cmd_string, cmd_key, exitFlag);
+
+
+    //FILE *fp = fopen("/Users/bijh/code/IsSameImage/tmp/final_features_1000.hashbin", "r");
+    //size_t dim = 512;
+    //size_t dataNum = 10000;
+    //
+    //float *feats = new float[dim * dataNum];
+    //fread(feats, sizeof(float), dim * dataNum, fp);
+    //fclose(fp);
     return 0;
 }
--- simplest_ffmpeg_helloworld/simplest_ffmpeg_helloworld.cpp
+++ simplest_ffmpeg_helloworld/simplest_ffmpeg_helloworld.cpp
@@ -1,200 +1,82 @@
-/**
- * 最简单的FFmpeg Helloworld程序
- * Simplest FFmpeg HelloWorld
- *
- * 雷霄骅 Lei Xiaohua
- * leixiaohua1020@126.com
- * 中国传媒大学/数字电视技术
- * Communication University of China / Digital TV Technology
- * http://blog.csdn.net/leixiaohua1020
- *
- *
- * 本程序是基于FFmpeg函数的最简单的程序。它可以打印出FFmpeg类库的下列信息：
- * Protocol:  FFmpeg类库支持的协议
- * AVFormat:  FFmpeg类库支持的封装格式
- * AVCodec:   FFmpeg类库支持的编解码器
- * AVFilter:  FFmpeg类库支持的滤镜
- * Configure: FFmpeg类库的配置信息
- *
- * This is the simplest program based on FFmpeg API. It can show following
- * informations about FFmpeg library:
- * Protocol:  Protocols supported by FFmpeg.
- * AVFormat:  Container format supported by FFmpeg.
- * AVCodec:   Encoder/Decoder supported by FFmpeg.
- * AVFilter:  Filters supported by FFmpeg.
- * Configure: configure information of FFmpeg.
- *
- */
+#include <iostream>
+#include <fstream>
 
-#include <stdio.h>
-
-#define __STDC_CONSTANT_MACROS
-
-#ifdef _WIN32
-//Windows
-extern "C"
-{
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
 #include "libavfilter/avfilter.h"
-};
-#else
-//Linux...
-#ifdef __cplusplus
-extern "C"
+
+AVCodecContext * CreateContext()
 {
-#endif
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavfilter/avfilter.h>
-#ifdef __cplusplus
-};
-#endif
-#endif
-
-//FIX
-struct URLProtocol;
-
-/**
- * Protocol Support Information
- */
-char *urlprotocolinfo() {
-
-    char *info = (char *) malloc(40000);
-    memset(info, 0, 40000);
-
-    av_register_all();
-
-    struct URLProtocol *pup = NULL;
-    //Input
-    struct URLProtocol **p_temp = &pup;
-    avio_enum_protocols((void **) p_temp, 0);
-    while ((*p_temp) != NULL) {
-        sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **) p_temp, 0));
-    }
-
-    pup = NULL;
-    //Output
-    avio_enum_protocols((void **) p_temp, 1);
-    while ((*p_temp) != NULL) {
-        sprintf(info, "%s[Out][%10s]\n", info, avio_enum_protocols((void **) p_temp, 1));
-    }
-
-    return info;
+    const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_AAC);
+    AVCodecContext *avCtx = avcodec_alloc_context3(codec);
+    return avCtx;
 }
 
-/**
- * AVFormat Support Information
- */
-char *avformatinfo() {
+int32_t DecodeBuffer(std::ostream &output, uint8_t *pInput, uint32_t cbInputSize, AVCodecContext * pAVContext)
+{
+    int32_t cbDecoded = 0;
 
-    char *info = (char *) malloc(40000);
-    memset(info, 0, 40000);
+    AVPacket avPacket;
+    av_init_packet(&avPacket);
 
-    av_register_all();
+    avPacket.size = cbInputSize;
+    avPacket.data = pInput;
 
-    AVInputFormat *if_temp = av_iformat_next(NULL);
-    AVOutputFormat *of_temp = av_oformat_next(NULL);
-    //Input
-    while (if_temp != NULL) {
-        sprintf(info, "%s[In ] %10s\n", info, if_temp->name);
-        if_temp = if_temp->next;
-    }
-    //Output
-    while (of_temp != NULL) {
-        sprintf(info, "%s[Out] %10s\n", info, of_temp->name);
-        of_temp = of_temp->next;
-    }
-    return info;
+    AVFrame * pDecodedFrame = av_frame_alloc();
+
+    int nGotFrame = 0;
+
+    cbDecoded = avcodec_decode_audio4(    pAVContext,
+                                          pDecodedFrame,
+                                          & nGotFrame,
+                                          & avPacket);
+
+    int data_size = av_samples_get_buffer_size( NULL,
+                                                pAVContext->ch_layout.nb_channels,
+                                                pDecodedFrame->nb_samples,
+                                                pAVContext->sample_fmt,
+                                                1);
+
+    output.write((const char*)pDecodedFrame->data[0],data_size);
+
+
+    av_frame_free(&pDecodedFrame);
+
+    return cbDecoded;
 }
 
-/**
- * AVCodec Support Information
- */
-char *avcodecinfo() {
-    char *info = (char *) malloc(40000);
-    memset(info, 0, 40000);
 
-    AVCodec *c_temp = av_codec_next(NULL);
+uint8_t * ReceiveBuffer( uint32_t * cbBufferSize)
+{
+    // TODO implement
 
-    while (c_temp != NULL) {
-        if (c_temp->decode != NULL) {
-            sprintf(info, "%s[Dec]", info);
-        } else {
-            sprintf(info, "%s[Enc]", info);
+    return NULL;
+}
+//https://stackoverflow.com/questions/24218527/how-to-decode-one-aac-frame-at-a-time-using-c
+int main(int argc, char *argv[])
+{
+    int nResult = 0;
+    AVCodecContext * pAVContext = CreateContext();
+    std::ofstream myOutputFile("audio.pcm",std::ios::binary);
+
+    while(1)
+    {
+        uint32_t cbBufferSize = 0;
+        uint8_t *pCompressedAudio = ReceiveBuffer( &cbBufferSize);
+
+        if(cbBufferSize && pCompressedAudio)
+        {
+            DecodeBuffer(myOutputFile,pCompressedAudio,cbBufferSize, pAVContext);
         }
-        switch (c_temp->type) {
-            case AVMEDIA_TYPE_VIDEO:
-                sprintf(info, "%s[Video]", info);
-                break;
-            case AVMEDIA_TYPE_AUDIO:
-                sprintf(info, "%s[Audio]", info);
-                break;
-            default:
-                sprintf(info, "%s[Other]", info);
-                break;
+        else
+        {
+            break;
         }
-
-        sprintf(info, "%s %10s\n", info, c_temp->name);
-
-        c_temp = c_temp->next;
     }
-    return info;
-}
 
-/**
- * AVFilter Support Information
- */
-char *avfilterinfo() {
-    char *info = (char *) malloc(40000);
-    memset(info, 0, 40000);
+    avcodec_close(pAVContext);
+    av_free(pAVContext);
 
-    avfilter_register_all();
-
-    AVFilter *f_temp = (AVFilter *) avfilter_next(NULL);
-
-    while (f_temp != NULL) {
-        sprintf(info, "%s[%15s]\n", info, f_temp->name);
-        f_temp = f_temp->next;
-    }
-    return info;
-}
-
-/**
- * Configuration Information
- */
-char *configurationinfo() {
-    char *info = (char *) malloc(40000);
-    memset(info, 0, 40000);
-
-    av_register_all();
-
-    sprintf(info, "%s\n", avcodec_configuration());
-
-    return info;
-}
-
-int main(int argc, char *argv[]) {
-    char *infostr = NULL;
-    infostr = configurationinfo();
-    printf("\n<<Configuration>>\n%s", infostr);
-    free(infostr);
-
-    infostr = urlprotocolinfo();
-    printf("\n<<URLProtocol>>\n%s", infostr);
-    free(infostr);
-
-    infostr = avformatinfo();
-    printf("\n<<AVFormat>>\n%s", infostr);
-    free(infostr);
-
-    infostr = avcodecinfo();
-    printf("\n<<AVCodec>>\n%s", infostr);
-    free(infostr);
-
-    infostr = avfilterinfo();
-    printf("\n<<AVFilter>>\n%s", infostr);
-    free(infostr);
-
-    return 0;
+    return nResult;
 }
